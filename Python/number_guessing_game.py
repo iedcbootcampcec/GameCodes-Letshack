85dc7176b574a55af7193b6d1f24d8607a9e2eed
lekshmi-02 <85623989+lekshmi-02@users.noreply.github.com>
2021-10-08 15:32:03 +0530

    Update number_guessing_game.py

--- Python/number_guessing_game.py
+++ Python/number_guessing_game.py
@@ -2,13 +2,14 @@ import random
 
 num = random.randint(1, 100)
 guess = None
+print("Let's Start the Game!!")
 
 while guess != num:
     guess = input("Guess a number between 1 and 100: ")
     guess = int(guess)
 
     if guess == num:
-        print("Congratulations! you won!")
+        print("Congratulations! You guessed it!")
         break
     elif guess>num:
         print("Your guess is greater!!")
